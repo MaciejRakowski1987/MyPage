964fbf3d6b3aac704c58f58511c6c0482ac67ae6
MaciejRaku <m.rakowski1987@gmail.com>
2018-02-13 14:21:41 +0100

    add slidebar
    slidebar
    animated hamburger icon

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,4 +1,20 @@
 $().ready(function() {
+  $('#navBar').on('click', function(event) {
+    event.preventDefault();
+    $('#sidebar').toggleClass('sidebar--visible');
+  })
+})
+
+$().ready(function() {
+  var $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+    // Do something else, like open/close menu
+  });
+})
+
+
+$().ready(function() {
   $('#fullpage').fullpage({
   	menu: '#menu',
 	navigation: true,
